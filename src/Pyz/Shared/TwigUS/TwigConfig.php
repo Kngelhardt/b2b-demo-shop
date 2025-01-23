eba9592f6ba1721c6af76614307540c2cd481268
Kristof Engelhardt <k.engelhardt@basecom.de>
2025-01-23 16:47:14 +0100

    level-exam-scout-task-1

---
+++ src/Pyz/Shared/TwigUS/TwigConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pyz\Shared\TwigUS;
+
+class TwigConfig extends \Spryker\Shared\Twig\TwigConfig
+{
+    public function getYvesThemeName(): string
+    {
+        return 'green';
+    }
+}
